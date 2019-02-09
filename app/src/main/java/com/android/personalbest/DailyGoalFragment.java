eb1f25586129a0a5f2439c9b5015347d08b46410
Jiawei-Tang-0820 <jit072@ucsd.edu>
2019-02-08 22:20:06 -0800

    added the button in daily goal view

--- app/src/main/java/com/android/personalbest/DailyGoalFragment.java
+++ app/src/main/java/com/android/personalbest/DailyGoalFragment.java
@@ -1,24 +1,45 @@
 package com.android.personalbest;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class DailyGoalFragment extends Fragment {
 
+    private Activity parentActivity;
+    private Button recordBtn;
 
-  public DailyGoalFragment() {
-    // Required empty public constructor
-  }
+    public DailyGoalFragment() {
+        // Required empty public constructor
+    }
 
-  @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                           Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_daily_goal, container, false);
-  }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_daily_goal, container, false);
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        parentActivity = getActivity();
+        recordBtn = parentActivity.findViewById(R.id.daily_goal_record_btn);
+        recordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onRecordBtnClicked(view);
+            }
+        });
+    }
+
+    private void onRecordBtnClicked(View v) {
+
+    }
 }
--- app/src/main/java/com/android/personalbest/WeeklyProgressFragment.java
+++ app/src/main/java/com/android/personalbest/WeeklyProgressFragment.java
@@ -27,6 +27,4 @@ public class WeeklyProgressFragment extends Fragment {
     }
 
 
-
-
 }
