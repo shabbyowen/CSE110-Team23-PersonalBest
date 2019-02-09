77ca404a3b96e76743946c95bc3ef8874f78fed4
Jiawei-Tang-0820 <jit072@ucsd.edu>
2019-02-08 21:56:53 -0800

    added empty fragments

---
+++ app/src/main/java/com/android/personalbest/DailyGoalFragment.java
@@ -0,0 +1,24 @@
+package com.android.personalbest;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DailyGoalFragment extends Fragment {
+
+
+  public DailyGoalFragment() {
+    // Required empty public constructor
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_daily_goal, container, false);
+  }
+
+}
--- app/src/main/java/com/android/personalbest/HomeScreenActivity.java
+++ app/src/main/java/com/android/personalbest/HomeScreenActivity.java
@@ -1,27 +1,40 @@
 package com.android.personalbest;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class HomeScreenActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+    private Fragment currentFragment;
+    private Fragment dailyGoalFragment;
+    private Fragment weeklyProgressFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    putFragment(
+                        dailyGoalFragment == null ?
+                        new DailyGoalFragment() :
+                        dailyGoalFragment);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    putFragment(
+                        weeklyProgressFragment == null ?
+                        new WeeklyProgressFragment() :
+                        weeklyProgressFragment);
                     return true;
 
             }
@@ -34,9 +47,31 @@ public class HomeScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        // init fragment manager
+        fragmentManager = getSupportFragmentManager();
+        putFragment(new DailyGoalFragment());
+
+        // stub
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    private void putFragment(Fragment fragment) {
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        // dont switch if the new fragment is the same
+        if (currentFragment == fragment) {
+            return;
+        }
+
+        // remove old fragment
+        if (currentFragment != null) {
+            fragmentTransaction.remove(currentFragment);
+        }
+
+        // add the new fragment and commit
+        fragmentTransaction.add(R.id.home_screen_container, fragment);
+        currentFragment = fragment;
+        fragmentTransaction.commit();
+    }
 }
