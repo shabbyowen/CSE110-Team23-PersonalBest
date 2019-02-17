7d216a1483c325134c94da4739bf3738c6a939bd
Haaris Tahir-Kheli <htahirkh@ucsd.edu>
2019-02-17 15:47:12 -0800

    About to change TimeMachine from static class to object class.

--- app/src/main/java/com/android/personalbest/util/TimeMachine.java
+++ app/src/main/java/com/android/personalbest/util/TimeMachine.java
@@ -1,12 +1,16 @@
 package com.android.personalbest.util;
 
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.TimeZone;
 
 public class TimeMachine {
 
     private static boolean timeFixed = false;
+    private static boolean timeSet = false;
     private static long time = 0;
+    private static final int CLOCK_CYCLE = 12;
+    private static final int HOURS_IN_DAY = 24;
 
     public static long nowMillis() {
         return DateCalculator.toLocalTime(nowCal().getTimeInMillis());
@@ -15,6 +19,10 @@ public class TimeMachine {
     public static Calendar nowCal() {
         Calendar cal = Calendar.getInstance();
         cal.setTimeZone(TimeZone.getDefault());
+
+        /*if (timeSet){
+            cal.setTimeInMillis(time);
+        }*/
         return cal;
     }
 
@@ -25,5 +33,21 @@ public class TimeMachine {
 
     public static void unfreezeTime() {
         timeFixed = false;
+    }
+
+    public static long setTime(long millis) {
+        return DateCalculator.toLocalTime(millis);
+    }
+
+    public static long getTimeInMillis(int year, int month, int day, int hour, int minutes, int seconds, boolean isPM) {
+        Calendar cal = Calendar.getInstance();
+        if (isPM) {
+            hour += CLOCK_CYCLE;
+            hour = hour % HOURS_IN_DAY;
+        }
+        cal.set(year, month, day, hour, minutes, seconds);
+        time = cal.getTimeInMillis();
+        timeSet = true;
+        return time;
     }
 }
