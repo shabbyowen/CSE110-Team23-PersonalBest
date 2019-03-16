e8e937e3e4a0fbb3b0da6e3f518861a8e18cc699
kra008 <kra008@ucsd.edu>
2019-03-15 17:47:10 -0700

    Fix FriendsListUI Espresso test to not be reliant on a Google Account (needed to use mock step service)

--- app/src/androidTest/java/com/cse110/personalbest/Activities/FriendListUITest.java
+++ app/src/androidTest/java/com/cse110/personalbest/Activities/FriendListUITest.java
@@ -12,6 +12,7 @@ import android.view.ViewParent;
 
 import com.cse110.personalbest.Factories.FriendServiceSelector;
 import com.cse110.personalbest.Factories.SessionServiceSelector;
+import com.cse110.personalbest.Factories.StepServiceSelector;
 import com.cse110.personalbest.Factories.StorageSolutionFactory;
 import com.cse110.personalbest.R;
 
@@ -49,6 +50,7 @@ public class FriendListUITest {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         Intent intent = new Intent();
+        intent.putExtra(HomeActivity.STEP_SERVICE_KEY_EXTRA, StepServiceSelector.MOCK_STEP_SERVICE_KEY);
         intent.putExtra(HomeActivity.FRIEND_SERVICE_KEY_EXTRA, FriendServiceSelector.MOCK_FRIEND_SERVICE_KEY);
         intent.putExtra(HomeActivity.SESSION_SERVICE_KEY_EXTRA, SessionServiceSelector.MOCK_SESSION_SERVICE_KEY);
         intent.putExtra(HomeActivity.STORAGE_SOLUTION_KEY_EXTRA, StorageSolutionFactory.MOCK_DICT_KEY);
